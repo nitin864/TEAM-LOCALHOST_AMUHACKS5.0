a21c7fccfdd8022dc4c7e8fec231386deafd88f8
codenitin864 <your-codenitin864-email@example.com>
2026-02-11 12:50:15 +0530

    designed sidebar and sticky-rapper for main page

---
+++ components/user-progress.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
+
+type Props = {
+    activeCourse:  typeof courses.$inferSelect;
+    hearts: number;
+    points: number;
+    hasActiveSubscription: boolean;
+};
+
+export const UserProgress = ({
+    activeCourse,
+    hearts,
+    points,
+    hasActiveSubscription,
+}: Props) => {
+    return (
+        <div className="flex items-center justify-between gap-x-2 w-full">
+            <Link href="/courses">
+                <Button variant="ghost">
+                    <Image
+                        src={activeCourse.imageSrc}
+                        alt={activeCourse.title}
+                        className="rounded-md border"
+                        width={32}
+                        height={32}
+                    />
+                </Button>
+            </Link>
+            <Link href="/shop">
+                <Button variant="ghost" className="text-orange-500 flex items-center">
+                    <Image
+                        src="/points.png"
+                        height={28}
+                        width={28}
+                        alt="Points"
+                        className="mr-2"
+                    />
+                    {points}
+                </Button>
+            </Link>
+            <Link href="/shop">
+                <Button variant="ghost" className="text-rose-500 flex items-center">
+                    <Image
+                        src="/heart.png"
+                        height={22}
+                        width={22}
+                        alt="Hearts"
+                        className="mr-2"
+                    />
+                    {hasActiveSubscription ? <InfinityIcon className="h-4 w-4 stroke-[3]"/> : hearts}
+                    {points}
+                </Button>
+            </Link>
+
+
+
+        </div>
+    );
+};
+
